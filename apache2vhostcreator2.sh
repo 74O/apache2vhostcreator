173589bc56467e8ed01cb535213659a73ced45fa
tao <paskuini@gmail.com>
2018-08-28 18:20:32 +0200

    .

--- apache2vhostcreator2.sh
+++ apache2vhostcreator2.sh
@@ -11,6 +11,7 @@ newHost(){
 
         # CREANDO ESTRUCTURA DE DIRECTORIOS
                 sudo mkdir -p $rutapache/$nombrehost/public
+                sudo mkdir -p $rutapache/$nombrehost/session
                 sudo chown -R $USER:$USER $rutapache/$nombrehost/public
                 sudo chmod -R 755 /var/www
         # CREANDO PAGINA DE EJEMPLO
@@ -20,9 +21,6 @@ newHost(){
         # CREANDO VIRTUAL HOST
                 sudo printf "<VirtualHost *:$puertohost>\n    ServerAdmin $sadmin\n    ServerName $nombrehost\n    ServerAlias $nombrehost\n    DocumentRoot $rutapache/$nombrehost/public\n" | sudo tee /etc/apache2/sites-available/$nombrehost.conf
                 sudo printf "    ErrorLog ${APACHE_LOG_DIR}/error.log\n    CustomLog ${APACHE_LOG_DIR}/access.log combined\n" | sudo tee -a /etc/apache2/sites-available/$nombrehost.conf
-                sudo printf "    <Directory \"$rutapache/$nombrehost/public\">\n" | sudo tee -a /etc/apache2/sites-available/$nombrehost.conf
-                sudo printf "        php_value session.save_path \"$rutapache/$nombrehost/\"\n" | sudo tee -a /etc/apache2/sites-available/$nombrehost.conf
-                sudo printf "    </Directory>" | sudo tee -a /etc/apache2/sites-available/$nombrehost.conf
                 sudo printf "\n\n    ErrorDocument 404 /404.html\n    ErrorDocument 401 /401.html\n    ErrorDocument 500 /50x.html\n    ErrorDocument 502 /50x.html\n    ErrorDocument 503 /50x.html\n    ErrorDocument 504 /50x.html\n</VirtualHost>\n\n\n" | sudo tee -a /etc/apache2/sites-available/$nombrehost.conf
                 sudo printf "Listen $puertohost\n" | sudo tee -a /etc/apache2/ports.conf
                 sudo a2ensite $nombrehost.conf
